60ad6d5570f1750133be6b641997f1c1b1324051
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2024-01-24 20:26:38 -0600

    add login and pages

--- padawans-portal/src/App.jsx
+++ padawans-portal/src/App.jsx
@@ -7,18 +7,19 @@ import Body from './components/Body'
 import Whiteboard from './components/Whiteboard'
 import Register from './components/forms/Register'
 import Users from './components/Users'
+import SocialPage from './pages/SocialPage'
+import FormPage from './pages/FormPage'
+import LandingPage from './pages/LandingPage'
+import Login from './components/forms/Login'
 
 export default function App() {
 
   return (
     <Container fluid data-bs-theme='dark' className='app'>
       <Header />
-      <Body>
-        {/* <Posts /> */}
-        {/* <Whiteboard /> */}
-        <Users />
-        {/* <Register /> */}
-      </Body>
+      <FormPage>
+        <Login />
+      </FormPage>
     </Container>
   )
 }
