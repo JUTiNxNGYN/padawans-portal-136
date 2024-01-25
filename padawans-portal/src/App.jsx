bbb2a9c08bf6fc394447af88ad453eb4b45de5da
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2024-01-24 19:29:12 -0600

    breaktime

--- padawans-portal/src/App.jsx
+++ padawans-portal/src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header'
 import Body from './components/Body'
 import Whiteboard from './components/Whiteboard'
 import Register from './components/forms/Register'
+import Users from './components/Users'
 
 export default function App() {
 
@@ -15,7 +16,8 @@ export default function App() {
       <Body>
         {/* <Posts /> */}
         {/* <Whiteboard /> */}
-        <Register />
+        <Users />
+        {/* <Register /> */}
       </Body>
     </Container>
   )
---
+++ padawans-portal/src/components/Users.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import Spinner from 'react-bootstrap/Spinner'
+ 
+export default function Users() {
+
+    const [ users, setUser ] = useState([])
+    
+    
+    useEffect(()=>{
+        (async () => {
+            const res = await fetch('http://127.0.0.1:5000/user')
+            if(res.ok){
+                const data = await res.json()
+                console.log(data);
+                setUser(data)
+            } else console.log('error'); 
+        })()
+    }, [])
+
+
+  if(users.length === 0){
+    return <Spinner />
+  }
+  
+  return (
+    <>
+        { users.map(user => {
+            return <p key={user.id}>{user.username}</p>
+        })}
+    </>
+  )
+}
