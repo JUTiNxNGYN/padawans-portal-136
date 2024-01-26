772a7a01b71222d0e5af82ac368e1ad324129155
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2024-01-25 19:35:22 -0600

    ugly nested routing in

--- padawans-portal/src/App.jsx
+++ padawans-portal/src/App.jsx
@@ -1,9 +1,7 @@
 import Container from 'react-bootstrap/Container'
 
-
-import Stack from 'react-bootstrap/Stack'
+import { Posts } from "./components/Posts"
 import Header from './components/Header'
-import Body from './components/Body'
 import Whiteboard from './components/Whiteboard'
 import Register from './components/forms/Register'
 import Users from './components/Users'
@@ -11,15 +9,21 @@ import SocialPage from './pages/SocialPage'
 import FormPage from './pages/FormPage'
 import LandingPage from './pages/LandingPage'
 import Login from './components/forms/Login'
+import { Route, Routes } from 'react-router-dom'
 
 export default function App() {
 
   return (
     <Container fluid data-bs-theme='dark' className='app'>
       <Header />
-      <FormPage>
-        <Login />
-      </FormPage>
+      <Routes>
+        <Route path='/' element={<LandingPage />}/>
+        <Route path='/login' element={<FormPage><Login /></FormPage>}/>
+        <Route path='/register' element={<FormPage><Register /></FormPage>}/>
+        <Route path='/users' element={<SocialPage><Users/></SocialPage>}/>
+        <Route path='/posts' element={<SocialPage><Posts/></SocialPage>}/>
+      </Routes>
+
     </Container>
   )
 }
