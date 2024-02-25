3bad811de4b4cb88bbef0a1746601c7a36bd6598
JUTiNxNGYN <JUTiNxNGYN@gmail.com>
2024-02-24 18:21:14 -0600

    adding post func

--- padawans-portal/src/components/Body.jsx
+++ padawans-portal/src/components/Body.jsx
@@ -1,18 +1,18 @@
 import Container from 'react-bootstrap/Container'
 import Stack from 'react-bootstrap/Stack'
+
 import Sidebar from './Sidebar'
-import { Posts } from "./Posts"
 
 export default function Body({ sidebar, children }) {
 
-  return (
-    <Container>
-        <Stack direction='horizontal'>
-            { sidebar && <Sidebar />}
-            <Container>
-            { children }
-            </Container>
-        </Stack>
-    </Container>
-  )
+    return (
+        <Container>
+            <Stack direction='horizontal'>
+               { sidebar && <Sidebar /> }
+                <Container className='center-content'>
+                    { children }
+                </Container>
+            </Stack>
+        </Container>
+    )
 }
--- padawans-portal/src/components/Users.jsx
+++ padawans-portal/src/components/Users.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 import Spinner from 'react-bootstrap/Spinner'
- 
+
 export default function Users() {
 
     const [ users, setUser ] = useState([])
@@ -10,18 +10,17 @@ export default function Users() {
             const res = await fetch('http://127.0.0.1:5000/user')
             if(res.ok){
                 const data = await res.json()
-                console.log(data);
+                
                 setUser(data)
-            } else console.log('error'); 
+            } else console.log('error');
         })()
     }, [])
-
-
-  if(users.length === 0){
-    return <Spinner />
-  }
-  
-  return (
+    
+    if(users.length === 0){
+        return <Spinner />
+    }
+    
+    return (
     <>
         { users.map(user => {
             return <p key={user.id}>{user.username}</p>
